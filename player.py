b29cde4cc8f2537cfc1b406488061fc31c4f4cf4
Jacob Rose <jacob@rostor.cloud>
2025-04-23 22:02:08 +0200

    added turn functionality

--- player.py
+++ player.py
@@ -10,8 +10,7 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-        
-        # in the player class
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -20,4 +19,15 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_a]: 
+            print(-1*dt)
+            self.rotate(-1*dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
